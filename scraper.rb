ad4179c3f8d18f6df3ab2f57999221b98c0a4c2b
sotos <s.karampatakis@gmail.com>
2017-11-22 23:15:56 +0200

    created some date extractors and added new fields start_date and end_date for each term

--- scraper.rb
+++ scraper.rb
@@ -6,20 +6,49 @@
 require 'scraperwiki'
 require 'mechanize'
 
+#a method to clean the period field
 def clean_string(string, last=-15, start=18)
 	string[start..last]
 end
 
+#a method to extract the dates from the period field
+def extract_dates(string)
+	str1 = ""
+    string.sub(/(\(.*?\))/) { str1 = $1 }
+    dates = str1[1..-2]
+end
+
+#a method to extract the start date from the period field
+def start_date(string)
+	string.split(" - ")[0]
+end
+
+#a method to extract the end date from the period field
+def end_date(string)
+	string.split(" - ")[1]
+end
+
 def scrap_mp(id, base)
+  #create the url
   mp_url = base + "?MpId=" + id
   agent = Mechanize.new
   page = agent.get(mp_url)
+  #get all table rows except footer
   rows = page.search('tbody > tr:not(.tablefooter)')
   rows.each_with_index do |(row,row_key), row_index|
     terms = row.search("td")
-    ScraperWiki.save_sqlite(["id"], {"id" => id + "|row|" + row_index.to_s,"hellenic_parliament_id" => id,
-       "period" => clean_string(terms[0].text, -16),
+    #clean period field
+    period = clean_string(terms[0].text, -16)
+    #get the period greek alphabet enumeration
+    suffix = period[0..3].rstrip[0..-2]
+    #extract the dates
+    dates = extract_dates(period)
+    ScraperWiki.save_sqlite(["id"], {"id" => id + "|period|" + suffix,"hellenic_parliament_id" => id,
+       "period" => period,
+       "period_letter" => suffix,
        "date" => clean_string(terms[1].text),
+       "start_date" => start_date(dates),
+       "end_date" => end_date(dates),
        "district" => clean_string(terms[2].text),
        "party" => clean_string(terms[3].text),
        "description" => clean_string(terms[4].text)
@@ -37,11 +66,16 @@ page = agent.get(url)
 # # Find somehing on the page using css selectors
 mp = page.search('select.mpsDropdown > option')
 mp.each_with_index do |(value, key), index|
+  # skip first 4 items as they contain empty elements
   if index < 4
     next
   end
+  #give some output
   p index, value.text
+  
+  #create the mp
   ScraperWiki.save_sqlite(["hellenic_parliament_id"], {"hellenic_parliament_id" => value.attr("value"), "full_name" => value.text})
+  #scrap terms info
   scrap_mp(value.attr("value"), url)
 end
 
