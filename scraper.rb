433daea737fc956ee24824dd0a79ec9122ed0e0e
skarampatakis <s.karampatakis@gmail.com>
2017-11-22 16:29:53 +0200

    added terms

--- scraper.rb
+++ scraper.rb
@@ -5,12 +5,30 @@
 
 require 'scraperwiki'
 require 'mechanize'
+
+def scrap_mp(id, base)
+  mp_url = base + "?MpId=" + id
+  agent = Mechanize.new
+  page = agent.get(mp_url)
+  rows = page.search('tbody > tr:not(.tablefooter)')
+  rows.each_with_index do |(row,row_key), row_index|
+    terms = row.search("td")
+    ScraperWiki.save_sqlite(["id"], {"id" => id + "|row|" + row_index.to_s,"hellenic_parliament_id" => id,
+       "period" => terms[0].text,
+       "date" => terms[1].text,
+       "district" => terms[2].text,
+       "party" => terms[3].text,
+       "description" => terms[4].text
+     }, 'terms')
+  end
+end
+
 #
- agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
 url = "http://www.hellenicparliament.gr/Vouleftes/Diatelesantes-Vouleftes-Apo-Ti-Metapolitefsi-Os-Simera"
- page = agent.get(url)
+page = agent.get(url)
 #
 # # Find somehing on the page using css selectors
 mp = page.search('select.mpsDropdown > option')
@@ -20,11 +38,10 @@ mp.each_with_index do |(value, key), index|
   end
   p index, value.text
   ScraperWiki.save_sqlite(["hellenic_parliament_id"], {"hellenic_parliament_id" => value.attr("value"), "full_name" => value.text})
+  scrap_mp(value.attr("value"), url)
 end
 
-def scrap_mp(url)
 
-end
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
 #
