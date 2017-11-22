1fdb06408561272bdca1e544c648113558c8bae6
Sotirios Karampatakis <s.karampatakis@gmail.com>
2017-11-22 14:21:14 +0200

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,16 +1,16 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+ require 'scraperwiki'
+ require 'mechanize'
 #
-# agent = Mechanize.new
+ agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+ page = agent.get("http://www.hellenicparliament.gr/Vouleftes/Diatelesantes-Vouleftes-Apo-Ti-Metapolitefsi-Os-Simera")
 #
 # # Find somehing on the page using css selectors
-# p page.at('div.content')
+ p page.at('div.content')
 #
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
